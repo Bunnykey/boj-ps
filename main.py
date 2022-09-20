40664f104f311053c38e97dbfd9cdaf7472e8e22
Bunnykey <40032007+Bunnykey@users.noreply.github.com>
2022-09-20 11:08:10 +0000

    1085

--- main.py
+++ main.py
@@ -1,34 +1,6 @@
-N, M = map(int, input().split())
-board = []
-for _ in range(N) :
-    board.append(input())
+x, y, w, h = map(int, input().split())
 
-w_case = "WB"*4 + "BW"*4
-b_case = "BW"*4 + "WB"*4
-min = 2174000
-cnt_1 = 0
-cnt_2 = 0
+x_min = min(w-x, x)
+y_min = min(h-y, y)
 
-for r in range(N-7):
-    for c in range(M-7):
-
-        cnt_1 = 0
-        cnt_2 = 0
-        sector = ""
-
-        for row in board[r:r+8] :
-            sector += row[c:c+8]
-            if len(sector) == 16:
-                for i in range(16) :
-                    if sector[i] != w_case[i]:
-                        cnt_1 += 1
-                    elif sector[i] != b_case[i] :
-                        cnt_2 += 1
-                sector = ""
-
-        if cnt_1 <= cnt_2 and cnt_1 < min :
-            min = cnt_1
-        elif cnt_2 < cnt_1 and cnt_2 < min :
-            min = cnt_2
-
-print(min)
+print(min(x_min, y_min))
